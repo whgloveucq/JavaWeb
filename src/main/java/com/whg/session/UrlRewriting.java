2b6de5d495df036bdb2da479f039434bda78a97d
Spring_whg <whgloveu@hotmail.com>
2018-11-27 18:04:36 +0800

    20181026

---
+++ src/main/java/com/whg/session/UrlRewriting.java
@@ -0,0 +1,18 @@
+package com.whg.session;
+/**
+ * Created by whg at 18-11-27
+ * Included in JavaWeb
+ * Go ahead ,do what you say and say what you do .
+ **/
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet(name="Top")
+public class UrlRewriting {
+}

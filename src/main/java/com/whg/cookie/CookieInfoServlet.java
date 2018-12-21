ac1b7e31843a284a50897d2cd2f6f4607f508327
Spring_whg <whgloveu@hotmail.com>
2018-12-21 17:23:30 +0800

    20181026

--- src/main/java/com/whg/cookie/CookieInfoServlet.java
+++ src/main/java/com/whg/cookie/CookieInfoServlet.java
@@ -19,6 +19,55 @@ public class CookieInfoServlet extends HttpServlet {
     private static final long serialVersionUID = -1660747338733704629L;
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    Cookie[] cookies=request.getCookies();
+    StringBuilder styles=new StringBuilder();
+    styles.append(".title{");
+    if(cookies !=null){
+        for (Cookie cookie:cookies){
+            String name=cookie.getName();
+            String value=cookie.getValue();
+            if(name.equals("titleFontSize")) {
+
+                styles.append("font-size:"+value+";");
+            }else  if(name.equals("titleFontWeight")){
+
+                styles.append("font-weight:"+value+";");
+
+            }else  if(name.equals("titleFontStyle")){
+
+                styles.append("font-style:"+value+";");
+
+            }
+
+
+
+        }
     }
+styles.append("}") ;
+
+response.setContentType("text/html");
+PrintWriter writer=response.getWriter();
+writer.print("<html><head>"+"<title>CookieInfo</title>" +"<style>"+styles.toString()+"</style>" +"</head><body>"
++ PreferenceServlet.MENU +"<div class='title'>" +"Session Management with Cookies:</div>");
+
+writer.print("<div class='title'>");
+//cookies will be null if there's no cookie/
+if(cookies==null){
+
+    writer.print("No cookie in this HTTPresponse");
+}else
+{writer.println("Cookies in this HTTP response");
+}
+for (Cookie cookie:cookies){
+   writer.println("<br/>" +cookie.getName()+":"+cookie.getValue());
+
+}
+
+writer.print("</div>");
+writer.print("</body></html>");
+    }
+
+
 }

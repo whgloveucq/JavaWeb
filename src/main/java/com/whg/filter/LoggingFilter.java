1023071c27860723f180e34529be7ad6600e9267
Spring_whg <whgloveu@hotmail.com>
2019-03-02 09:58:02 +0800

    20181026

--- src/main/java/com/whg/filter/LoggingFilter.java
+++ src/main/java/com/whg/filter/LoggingFilter.java
@@ -14,17 +14,18 @@ import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.annotation.WebInitParam;
 import javax.servlet.http.HttpServletRequest;
-@WebFilter(filterName = "LoggingFilter",urlPatterns = {"/*"},initParams = {@WebInitParam(name="logFileName",value="log.txt"),@WebInitParam(name="prefix",value = "URI:")})
+//@WebFilter(filterName = "LoggingFilter",urlPatterns = {"/*"},initParams = {@WebInitParam(name="logFileName",value="log.txt"),@WebInitParam(name="prefix",value = "URI:")})
 public class LoggingFilter implements Filter{
 private PrintWriter logger;
 
 private String prefix;
+private  String appPath;
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
         prefix=filterConfig.getInitParameter("prefix");
         String logFileName=filterConfig.getInitParameter("logFileName");
-        String appPath=filterConfig.getServletContext().getRealPath("/");
+         appPath=filterConfig.getServletContext().getRealPath("/");
 //        without path info in logFileName,the log file will be created in $TOMCAT_HOME/bin
  System.out.println("logFileName:" + logFileName);
 
@@ -50,8 +51,9 @@ logger=new PrintWriter(new File(appPath,logFileName));
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
 System.out.println("LoggingFilter.doFilter");
+System.out.println(appPath);
 HttpServletRequest httpServletRequest=(HttpServletRequest)request;
-logger.println(new Date() + "" + prefix + httpServletRequest.getRequestURI());
+logger.println(new Date() + " " + prefix + httpServletRequest.getRequestURI());
 logger.flush();
 filterChain.doFilter(request,response);
     }

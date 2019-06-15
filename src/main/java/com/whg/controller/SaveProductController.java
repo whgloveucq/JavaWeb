5a19e184fe02eeecb2cabe89c6ebd0a881e05f2b
whgloveucq <whgloveu@hotmail.com>
2019-06-15 08:08:30 +0800

    20181222

--- src/main/java/com/whg/controller/SaveProductController.java
+++ src/main/java/com/whg/controller/SaveProductController.java
@@ -1,10 +1,12 @@
 package com.whg.controller;
+import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.whg.model.Product;
 import com.whg.model.Product1;
 import com.whg.form.ProductForm;
+import com.whg.validator.ProductValidator;
 
 
 /**
@@ -21,18 +23,30 @@ public class SaveProductController implements Controller  {
         productForm.setName(request.getParameter("name"));
         productForm.setDescription(request.getParameter("description"));
         productForm.setPrice(request.getParameter("price"));
-        //create model
-        Product1 product= new Product1();
-        product.setName(productForm.getName());
-        product.setDescription(productForm.getDescription());
-        try{
-            product.setPrice(Float.parseFloat(productForm.getPrice()));
-        }catch (NumberFormatException e){
+        //validate ProductForm
+        ProductValidator productValidator=new ProductValidator();
+        List<String> errors=productValidator.validate(productForm);
+        if(errors.isEmpty()) {
 
+            //create model
+            Product1 product = new Product1();
+            product.setName(productForm.getName());
+            product.setDescription(productForm.getDescription());
+            try {
+                product.setPrice(Float.parseFloat(productForm.getPrice()));
+            } catch (NumberFormatException e) {
+
+            }
+            //insert code to add product to the database
+            request.setAttribute("product", product);
+            return "/WEB-INF/jsp/ProductDetails.jsp";
         }
-        //insert code to add product to the database
-        request.setAttribute("product",product);
-        return "/WEB-NF/jsp/ProductDetails.jsp" ;
+        else{
+            //store errors and form in a scope variable for the view
+            request.setAttribute("errors" ,errors);
+            request.setAttribute("form",productForm);
+            return "/WEB-INF/jsp/ProductForm.jsp" ;
+        }
 
 
     }
--- web/WEB-INF/jsp/ProductForm.jsp
+++ web/WEB-INF/jsp/ProductForm.jsp
@@ -6,15 +6,27 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c"  uri="http://java.sun.com/jsp/jstl/core" %>
 <html>
 <head>
     <title>Add Product Form</title>
     <style type="text/css">@import url(/css/main.css);</style>
 </head>
 <body>
-<div id="global" >
-test
-    <form action="product_save.action" method="post">
+<c:if test="${requestScope.errors != null }">
+    <p id="errors">
+        Error(s)!
+        <ul>
+      <c:forEach var="error" items="${requestScope.errors}">
+          <li> ${error} </li>
+      </c:forEach>
+
+        </ul>
+    </p>
+</c:if>
+<br/>
+<div id="global">
+    <form action="product_savee.action" method="post" >
     <fieldset>
         <legend>Add a product</legend>
         <p>
@@ -34,7 +46,6 @@ test
         <p id="buttons">
             <input id="reset" type="reset" tabindex="4">
             <input id="submit" type="submit" tabindex="5" value="Add Product" >
-
 
 
         </p>
